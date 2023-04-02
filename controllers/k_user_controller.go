0a496ac08b093569576db5ff220bbefe29f27730
satriaprayoga <satria.prayoga@gmail.com>
2023-04-02 22:17:54 +0700

    user controller

--- controllers/k_user_controller.go
+++ controllers/k_user_controller.go
@@ -7,9 +7,11 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/mitchellh/mapstructure"
 	"github.com/satriaprayoga/cukurin-barber/form"
 	"github.com/satriaprayoga/cukurin-barber/interfaces/services"
 	"github.com/satriaprayoga/cukurin-barber/middlewares"
+	"github.com/satriaprayoga/cukurin-barber/models"
 	"github.com/satriaprayoga/cukurin-barber/pkg/response"
 )
 
@@ -22,10 +24,15 @@ func NewKUserController(e *echo.Echo, k services.IKUserService) {
 	r := e.Group("/barber/users")
 	r.Use(middlewares.JWT)
 	r.GET("/:id", cont.GetDataBy)
+	r.GET("", cont.GetList)
+	r.POST("", cont.Create)
+	r.POST("/change_password", cont.ChangePassword)
+	r.PUT("/:id", cont.Update)
+	r.DELETE("/:id", cont.Delete)
 
 }
 
-func (c *KUserController) GetDataBy(e echo.Context) error {
+func (u *KUserController) GetDataBy(e echo.Context) error {
 	ctx := e.Request().Context()
 	if ctx == nil {
 		ctx = context.Background()
@@ -44,9 +51,149 @@ func (c *KUserController) GetDataBy(e echo.Context) error {
 	if err != nil {
 		return resp.Response(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
 	}
-	data, err := c.kuserUseCase.GetDataBy(ctx, claims, ID)
+	data, err := u.kuserUseCase.GetDataBy(ctx, claims, ID)
 	if err != nil {
 		return resp.Response(http.StatusInternalServerError, fmt.Sprintf("%v", err), nil)
 	}
 	return resp.Response(http.StatusOK, "OK", data)
+}
+
+func (u *KUserController) GetList(e echo.Context) error {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	var (
+		resp         = response.Resp{R: e}
+		paramQuery   = models.ParamList{}
+		responseList = models.ResponseModelList{}
+		err          error
+	)
+	httpCode, errMsg := form.BindAndValid(e, &paramQuery)
+	if httpCode != 200 {
+		return resp.ResponseErrorList(http.StatusBadRequest, errMsg, responseList)
+	}
+	claims, err := form.GetClaims(e)
+	if err != nil {
+		return resp.ResponseErrorList(http.StatusBadRequest, fmt.Sprintf("%v", err), responseList)
+	}
+
+	responseList, err = u.kuserUseCase.GetList(ctx, claims, paramQuery)
+	if err != nil {
+		return resp.ResponseErrorList(response.GetStatusCode(err), fmt.Sprintf("%v", err), responseList)
+	}
+	return resp.Response(http.StatusOK, "", responseList)
+}
+
+func (u *KUserController) Create(e echo.Context) error {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	var (
+		resp     = response.Resp{R: e}
+		kuser    models.KUser
+		userForm models.AddUser
+	)
+
+	httpCode, errMsg := form.BindAndValid(e, &userForm)
+	if httpCode != 200 {
+		return resp.ResponseError(http.StatusBadRequest, errMsg, nil)
+	}
+	err := mapstructure.Decode(userForm, &kuser)
+	if err != nil {
+		return resp.ResponseError(http.StatusInternalServerError, fmt.Sprintf("%v", err), nil)
+	}
+	claims, err := form.GetClaims(e)
+	if err != nil {
+		return resp.ResponseError(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
+	}
+	err = u.kuserUseCase.Create(ctx, claims, &kuser)
+	if err != nil {
+		return resp.ResponseError(response.GetStatusCode(err), fmt.Sprintf("%v", err), nil)
+	}
+	return resp.Response(http.StatusCreated, "Ok", kuser)
+}
+
+func (u *KUserController) Update(e echo.Context) error {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	var (
+		resp     = response.Resp{R: e}
+		err      error
+		id       = e.Param("id")
+		userForm = models.UpdateUser{}
+	)
+
+	OwnerID, err := strconv.Atoi(id)
+	if err != nil {
+		return resp.ResponseError(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
+	}
+	claims, err := form.GetClaims(e)
+	if err != nil {
+		return resp.ResponseError(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
+	}
+	httpCode, errMsg := form.BindAndValid(e, &userForm)
+	if httpCode != 200 {
+		return resp.ResponseError(http.StatusBadRequest, errMsg, nil)
+	}
+	err = u.kuserUseCase.Update(ctx, claims, OwnerID, userForm)
+	if err != nil {
+		return resp.ResponseError(response.GetStatusCode(err), fmt.Sprintf("%v", err), nil)
+	}
+	return resp.Response(http.StatusCreated, "Ok", nil)
+
+}
+
+func (u *KUserController) ChangePassword(e echo.Context) error {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	var (
+		resp     = response.Resp{R: e}
+		err      error
+		userForm = models.ChangePassword{}
+	)
+	claims, err := form.GetClaims(e)
+	if err != nil {
+		return resp.ResponseError(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
+	}
+	httpCode, errMsg := form.BindAndValid(e, &userForm)
+	if httpCode != 200 {
+		return resp.ResponseError(http.StatusBadRequest, errMsg, nil)
+	}
+	err = u.kuserUseCase.ChangePassword(ctx, claims, userForm)
+	if err != nil {
+		return resp.ResponseError(response.GetStatusCode(err), fmt.Sprintf("%v", err), nil)
+	}
+	return resp.Response(http.StatusCreated, "Ok", nil)
+
+}
+
+func (u *KUserController) Delete(e echo.Context) error {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	var (
+		resp = response.Resp{R: e}
+		id   = e.Param("id")
+	)
+	ID, err := strconv.Atoi(id)
+	if err != nil {
+		return resp.ResponseError(http.StatusBadRequest, fmt.Sprintf("%v", err), nil)
+	}
+	err = u.kuserUseCase.Delete(ctx, ID)
+	if err != nil {
+		return resp.ResponseError(http.StatusInternalServerError, fmt.Sprintf("%v", err), nil)
+	}
+	return resp.Response(http.StatusOK, "Ok", nil)
+
 }
--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,6 +1,8 @@
 package response
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/satriaprayoga/cukurin-barber/models"
 	"github.com/satriaprayoga/cukurin-barber/pkg/logging"
@@ -56,4 +58,25 @@ func (e Resp) ResponseList(httpCode int, errMsg string, data models.ResponseMode
 	logger.Info(string(utils.Stringify(data)))
 	return e.R.JSON(httpCode, data)
 	// return string(util.Stringify(response))
+}
+
+func GetStatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+
+	switch err {
+	case models.ErrInternalServerError:
+		return http.StatusInternalServerError
+	case models.ErrNotFound:
+		return http.StatusNotFound
+	case models.ErrConflict:
+		return http.StatusConflict
+	case models.Unauthorized:
+		return http.StatusUnauthorized
+	case models.ErrInvalidLogin:
+		return http.StatusUnauthorized
+	default:
+		return http.StatusInternalServerError
+	}
 }
